3e0da047769d709630b9693f432adc5bb9cefe9a
Makha.D <87713231+mbd89@users.noreply.github.com>
2024-05-25 17:20:06 +0200

    Add files via upload

---
+++ template-strings.js
@@ -0,0 +1,12 @@
+/*
+Task
+Your task is to return the correct string using the Template String Feature.
+Input
+Two Strings, no validation is needed.
+Output
+You must output a string containing the two strings with the word ```' are '```
+*/
+
+var templateStrings = function(noun, adjective) {
+    return noun + ` are ` + adjective
+  }
