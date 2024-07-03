deac55767a120cec73f9ae805ce028a5b4ae8297
Makha.D <87713231+mbd89@users.noreply.github.com>
2024-07-03 08:27:27 +0200

    Add files via upload

---
+++ reduceGrow.js
@@ -0,0 +1,18 @@
+/*
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+*/
+
+
+
+
+
+function grow(x){
+  let s = 1;
+  for (let i=0; i<x.length; i++){
+    s= s*x[i]
+  }
+  return s
+  }
+
