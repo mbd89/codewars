b21f4795b0a29dcc637ae5787853daf3ea4c96fd
Makha.D <87713231+mbd89@users.noreply.github.com>
2024-09-30 17:40:19 +0000

    Add files via upload

---
+++ trainingJSObjects.js
@@ -0,0 +1,25 @@
+
+/*
+Task
+Give you a function animal, accept 1 parameter:obj like this:
+
+{name:"dog",legs:4,color:"white"}
+and return a string like this:
+
+"This white dog has 4 legs."
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+*/
+
+
+
+
+
+
+function animal(obj){
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
+  }
+    
+  
+
+ 
+ 
